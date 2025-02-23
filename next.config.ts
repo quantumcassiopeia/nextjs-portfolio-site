632be8afe896542f004003edab2d90845ddcc7d2
Quantum <russewelt@gmail.com>
2025-02-22 21:13:23 -0300

    nextjs config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? "nextjs-portfolio-site" : "",
+  output: "export",
+  images: {
+    unoptimized: false,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
